a4e84850f6c517dc312f50e6e0b5e23d146fdd80
Ubuntu <ubuntu@ip-172-31-18-172.ap-northeast-1.compute.internal>
2013-07-06 04:14:04 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Buythislook::Application.config.secret_key_base = '1435f20dc5178ef634d7238b22aa6b45bdc897f0d8af9cbf429d8144d5d81ac52efd1458b242c155b1fcbd17caf75a4e6c08bc8b8dbf28d526b5897d3da20a06'
